8be4ddcd6abd8a5df6b1249da2df9e23fd8c14a4
prasuksb2007-blip <prasuk.sb2007@gmail.com>
2026-08-07 10:22:38 +0530

    practice purpose only

---
+++ practice.py
@@ -0,0 +1,10 @@
+memo={}
+def fib(n):
+    if n in memo:
+        return memo[n]
+    if n <= 1:
+        return n
+    memo[n] = fib(n-1) + fib(n-2)
+    return memo[n]
+
+print(fib(6))  # Output: 8
